a2a79bdd65bad6de17bf4935640901b88434b4db
thiago da silva <thg021@gmail.com>
2023-04-22 09:47:54 -0300

    feat: add entity product story catalog

---
+++ src/modules/store-catolog/domain/product.entity.ts
@@ -0,0 +1,35 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root-interface";
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+
+type ProductProps = {
+  id: Id;
+  name: string;
+  description: string;
+  salesPrice: number;
+};
+
+export default class Product extends BaseEntity implements AggregateRoot {
+  #name: string;
+  #description: string;
+  #salesPrice: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+    this.#name = props.name;
+    this.#description = props.description;
+    this.#salesPrice = props.salesPrice;
+  }
+
+  get name(): string {
+    return this.#name;
+  }
+
+  get description(): string {
+    return this.#description;
+  }
+
+  get salesPrice(): number {
+    return this.#salesPrice;
+  }
+}
